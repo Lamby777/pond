d07ba2665908748acf2ed56f863079ac9a4da28e
Cherry <cherry@sparklet.org>
2024-10-27 21:51:20 -0400

    allow top-level await + moved index.html inside client dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    root: "src/client",
     plugins: [vue()],
+
+    esbuild: {
+        supported: {
+            'top-level-await': true,
+        },
+    },
+
     css: {
         preprocessorOptions: {
             scss: {
