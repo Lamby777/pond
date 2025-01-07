bfb8432c45ed0f370bdcb0273e0ff5d4daef6580
Cherry <cherry@sparklet.org>
2025-01-07 06:56:58 -0500

    Revert "rm unused consts"
    This reverts commit 31a60466443b9a48c31c6b57d4391cf73546875d.

--- src/server/consts.ts
+++ src/server/consts.ts
@@ -2,6 +2,11 @@ import { homedir } from "os";
 import path from "path";
 import bcrypt from "bcrypt";
 
+export const HOME_DIR = homedir();
+export const APP_FOLDER = process.env.NODE_ENV === "production" ? "/srv/pond/" :
+    path.join(HOME_DIR, "pond/");
+
+
 export const PORT = 1998;
 export const FILE_UPLOAD_LIMIT = 1_000_000_000; // 1 GB
 export const FILE_UPLOAD_DEST = "/tmp/uploads/";
